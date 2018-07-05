9a864f5124446c620f7a0e3a50e2643d24e2cc8a
k1rh4 <k1rh4.lee@gmail.com>
2018-07-05 21:58:18 +0900

    Fix bug

--- Docker_debug/run_docker.sh
+++ Docker_debug/run_docker.sh
@@ -9,7 +9,6 @@ PORT="-p 2222:22"
 SHARED=
 OPTION=" --rm "
 #OPTION="--rm  --cap-add=SYS_PTRACE --ulimit core=-1 --security-opt seccomp=unconfined "
-#OPTION=" --rm -v /home/k1rh4/GITHUB/samsungctf18/attack/HEAP_BY_HEAP/deploy/tmp:/tmp"
 
 sudo docker run -idt --name $NAME $PORT $SHRED $OPTION $NAME:1.0 /bin/bash
 #sudo docker run -d --name $NAME $PORT $SHRED $OPTION $NAME:1.0 /bin/sh
