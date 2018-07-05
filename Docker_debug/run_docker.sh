fa6a43eb056eb0076f26ed55f47ec0bd2aab0b91
k1rh4 <k1rh4.lee@gmail.com>
2018-07-05 21:55:03 +0900

    Ubuntu 16.04 docker

---
+++ Docker_debug/run_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+NAME="ubuntu2"
+sudo docker kill $NAME
+sudo docker rm $NAME
+sudo docker build --tag $NAME:1.0 ./
+
+PORT="-p 2222:22"
+
+SHARED=
+OPTION=" --rm "
+#OPTION="--rm  --cap-add=SYS_PTRACE --ulimit core=-1 --security-opt seccomp=unconfined "
+#OPTION=" --rm -v /home/k1rh4/GITHUB/samsungctf18/attack/HEAP_BY_HEAP/deploy/tmp:/tmp"
+
+sudo docker run -idt --name $NAME $PORT $SHRED $OPTION $NAME:1.0 /bin/bash
+#sudo docker run -d --name $NAME $PORT $SHRED $OPTION $NAME:1.0 /bin/sh
+
+sudo docker attach $NAME
