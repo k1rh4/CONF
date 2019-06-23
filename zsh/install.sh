30fb5f87f2cd5df32f705aafa7421d4d5e731317
k1rh4 <k1rh4.lee@gmail.com>
2019-06-22 19:25:56 -0700

    Add thema

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh 
-apt-get install zsh
+sudo apt-get install -y zsh
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-echo "Recommand THEMA = ys" 
+cp ./k1rh4.zsh-theme ~/.oh-my-zsh/themes/
+
+echo "Recommand THEMA = k1rh4" 
 
 
