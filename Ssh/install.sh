4c658dc6a01ce9bceb5da0fdbdd0476f3e9e1b69
k1rh4 <k1rh4.lee@gmail.com>
2021-03-22 10:48:53 +0900

    Add rsa key gen logic

--- Ssh/install.sh
+++ Ssh/install.sh
@@ -1,4 +1,12 @@
 cp config ~/.ssh/config
 chmod 440 ~/.ssh/config
-ssh-keyhen 
+
+ssh-keyhen -t rsa
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/id_rsa
+chmod 644 ~/.ssh/id_rsa.pub
+cat ~/.ssh/id_rsa > authorized_keys
+chmod 644 authorized_keys
+scp authorized_keys [remoteserver]:~/.ssh/
+
 
