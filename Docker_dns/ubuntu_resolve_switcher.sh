a72104956fc4a7f88a765e2c02700ec1dc2cfeaf
k1rh4 <k1rh4.lee@gmail.com>
2022-03-25 08:38:35 +0900

    Add file for ubuntu resolve switcher

--- Docker_dns/ubuntu_resolve_switcher.sh
+++ Docker_dns/ubuntu_resolve_switcher.sh
@@ -3,8 +3,8 @@ if [ "$1"="0" ]; then
     systemctl stop systemd-resolved
     systemctl disable systemd-resolved
 elif [ "$1"="1" ]; then 
-    systemctl start systemd-resolved
     systemctl enable systemd-resolved
+    systemctl start systemd-resolved
 else
     echo "usage ./ubuntu_resolve_switcher.sh 1"
     echo "usage ./ubuntu_resolve_switcher.sh 0"
