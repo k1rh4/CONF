3af17275d627496c2ff1518c83294e87ce4825ba
k1rh4 <k1rh4.lee@gmail.com>
2022-05-30 07:27:48 +0900

    hahah setting

--- tmux/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo apt-get install -y tmux
-
-cp ./powerline-theme.tmux ~/.powerline-theme.tmux
-cp ./tmux.conf ~/.tmux.conf
-
-
-
--- tmux/mac_fontfatch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-brew update
-brew install -vd fontforge
-
-fontforge -scrpt /path/to/fontfatcher MyFontFile.tff
-
